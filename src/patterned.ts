26d20711825998d3906025464585ce7205040b7c
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-23 04:00:39 +0300

    Update patterned.ts

--- src/patterned.ts
+++ src/patterned.ts
@@ -1,20 +1,22 @@
-import {Func} from './precepts';
-import {Identity} from './util';
+import type { Func } from "./precepts";
+import type { Identity } from "./util";
 
 /**
  * Template that follows a particular pattern.
  */
-export type PatternedTemplate<F extends Func> = {[type: string]: F};
+export type PatternedTemplate<F extends Func> = { [type: string]: F };
 
 /**
  * Patched Template helper.
  */
 type PatchedTemplate<T extends PatternedTemplate<F>, F extends Func> = {
-    [P in keyof T]: (...args: Parameters<T[P]>) => Identity<ReturnType<T[P]> & ReturnType<F>>;
-}
-export function patterned<
-    T extends PatternedTemplate<F>,
-    F extends Func,
->(_constraint_: F, v: T) {
-    return v as PatchedTemplate<T, F>;
+	[P in keyof T]: (
+		...args: Parameters<T[P]>
+	) => Identity<ReturnType<T[P]> & ReturnType<F>>;
+};
+export function patterned<T extends PatternedTemplate<F>, F extends Func>(
+	_constraint_: F,
+	v: T,
+) {
+	return v as PatchedTemplate<T, F>;
 }
