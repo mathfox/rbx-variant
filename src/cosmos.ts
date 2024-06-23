52e593c3506553f6aef5e49bee9e5f994c659f9b
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-23 03:10:14 +0300

    Create cosmos.ts

---
+++ src/cosmos.ts
@@ -0,0 +1,61 @@
+import {IsTypeFunc, isTypeImpl} from './isType';
+import {MatchFuncs, matchImpl} from './match';
+import {VariantFuncs, variantImpl} from './variant';
+import {TypesFunc, typesImpl} from './types';
+import {FlagsFunc, flagsImpl} from './flags';
+import {IsOfVariantFunc, isOfVariantImpl} from './isOfVariant';
+import {RemoteFuncs, remoteImpl} from './remote';
+import {TypedFunc, typedImpl} from './typed';
+import {MatcherFunc, matcherImpl} from './matcher';
+
+/**
+ * All the functions in the library that pivot on a certain discriminant.
+ */
+export interface VariantCosmos<
+    K extends string,
+> extends
+    IsOfVariantFunc<K>,
+    IsTypeFunc<K>,
+    FlagsFunc<K>,
+    MatchFuncs<K>,
+    MatcherFunc<K>,
+    RemoteFuncs<K>,
+    TypedFunc<K>,
+    TypesFunc<K>,
+    VariantFuncs<K>
+{
+    key: K;
+}
+
+export interface VariantCosmosConfig<K extends string> {
+    /**
+     * The discriminant to be used by these functions.
+     */
+    key: K
+}
+
+/**
+ * Generate a series of functions to work off a given key.
+ * @param config the key to use.
+ * @template K discriminant as string literal.
+ * @returns `VariantCosmos<K>`
+ */
+export function variantCosmos<
+    K extends string,
+>({key}: VariantCosmosConfig<K>): VariantCosmos<K> {
+    const {isType} = isTypeImpl(key);
+    const {flags} = flagsImpl(key);
+
+    return {
+        key,
+        isType,
+        flags,
+        ...isOfVariantImpl(key),
+        ...matchImpl(key),
+        ...matcherImpl(key),
+        ...remoteImpl(key),
+        ...typedImpl(key),
+        ...typesImpl(key),
+        ...variantImpl(key),
+    }
+}
