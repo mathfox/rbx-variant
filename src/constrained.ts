4ba23139512b6f5c005f2fe87a26b06ca3dc6384
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-23 03:09:31 +0300

    Update constrained.ts

--- src/constrained.ts
+++ src/constrained.ts
@@ -1,17 +1,21 @@
-import {Func} from './precepts';
-import {Identity} from './util';
+import { Func } from "./precepts";
+import { Identity } from "./util";
 
 /**
  * Constrained template.
  */
-export type ConstrainedTemplate<F extends Func> = {[type: string]: (...args: [...Parameters<F>, ...any[]]) => ReturnType<F>}
+export type ConstrainedTemplate<F extends Func> = {
+	[type: string]: (...args: [...Parameters<F>, ...any[]]) => ReturnType<F>;
+};
 
 /**
  * Patched Template helper.
  */
 type PatchedTemplate<T extends ConstrainedTemplate<F>, F extends Func> = {
-    [P in keyof T]: (...args: Parameters<T[P]>) => Identity<ReturnType<T[P]> & ReturnType<F>>;
-}
+	[P in keyof T]: (
+		...args: Parameters<T[P]>
+	) => Identity<ReturnType<T[P]> & ReturnType<F>>;
+};
 
 /**
  * Constrained variant. A variant where each form abides by a given constraint - handles these
@@ -22,9 +26,9 @@ type PatchedTemplate<T extends ConstrainedTemplate<F>, F extends Func> = {
  * @param v
  * @returns
  */
-export function constrained<
-    T extends ConstrainedTemplate<F>,
-    F extends Func,
->(_constraint_: F, v: T) {
-    return v as PatchedTemplate<T, F>;
+export function constrained<T extends ConstrainedTemplate<F>, F extends Func>(
+	_constraint_: F,
+	v: T,
+) {
+	return v as PatchedTemplate<T, F>;
 }
