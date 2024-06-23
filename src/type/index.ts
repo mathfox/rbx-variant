b41da19d24caac8e82bd812a0967fa3e7eb810f1
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-23 04:09:30 +0300

    Create index.ts

---
+++ src/type/index.ts
@@ -0,0 +1,27 @@
+import { variantCosmos } from "../cosmos";
+
+export const {
+	descope,
+	flags,
+	inferTypes,
+	isOfVariant,
+	isType,
+	match,
+	matcher,
+	ofLiteral,
+	onLiteral,
+	otherwise,
+	partial,
+	prematch,
+	remote,
+	scoped,
+	sequence,
+	lookup,
+	typed,
+	types,
+	variant,
+	variantList,
+	variantModule,
+	variation,
+	withFallback,
+} = variantCosmos({ key: "type" });
