ab5ac808f1276264b439f4f774576a8a47bfee82
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-23 04:15:40 +0300

    Create variant.tools.ts

---
+++ src/variant.tools.ts
@@ -0,0 +1,28 @@
+/**
+ * Describe the fields of the variation.
+ *
+ * When used creates a function of type `(input: T) => T & {type: 'literal'}`
+ * @param defaults set some default values for the object. Note this does *not* adjust the return type.
+ */
+export function fields<T extends {}>(defaults: Partial<T> = {}) {
+    return (...args: {} extends T ? ([input: T] | []) : [input: T]) => {
+        const [arg] = args;
+        return {
+            ...defaults,
+            ...arg,
+        } as T;
+    }
+}
+
+/**
+ * Take a single variable of type T and store as 'payload'
+ */
+export function payload<T>(_example?: T) {
+    return (payload: T) => ({payload})
+}
+
+
+/**
+ * Create an empty variation (`{type: 'literal'}`).
+ */
+export const nil = () => ({});
