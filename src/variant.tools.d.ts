ff31061efc8d8e999ee7d7c2557ca2ae0d17cc18
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-20 12:52:17 +0300

    refactor variant tools

--- src/variant.tools.d.ts
+++ src/variant.tools.d.ts
@@ -2,27 +2,17 @@
  * Describe the fields of the variation.
  *
  * When used creates a function of type `(input: T) => T & {type: 'literal'}`
+ *
  * @param defaults set some default values for the object. Note this does *not* adjust the return type.
  */
-export function fields<T extends {}>(defaults: Partial<T> = {}) {
-	return (...args: {} extends T ? [input?: T] : [input: T]) => {
-		const [arg] = args;
-
-		return {
-			...defaults,
-			...arg,
-		} as T;
-	};
-}
+export function fields<T extends {}>(defaults?: Partial<T>): (...args: {} extends T ? [input?: T] : [input: T]) => T;
 
 /**
  * Take a single variable of type T and store as 'payload'
  */
-export function payload<T>(_example?: T) {
-	return (payload: T) => ({ payload });
-}
+export function payload<T>(): () => { payload: T };
 
 /**
  * Create an empty variation (`{type: 'literal'}`).
  */
-export const none = () => ({});
+export function none(): () => {};
